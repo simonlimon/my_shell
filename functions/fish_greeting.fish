c8fa5d9c07ec485784bd919e7ffb28195ab54ce4
Simon Posada Fishman <simon.posada29@gmail.com>
2016-04-19 10:51:43 -0700

    Improve greeting

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
 	printf "%s %s %s\n" (tput setaf 70)(whoami) (tput setaf 7)'at' (tput setaf 3)(hostname)
     echo (tput setaf 7)""
-    echo (fortune -s)
+    cowsay -b (fortune -s)
 end
